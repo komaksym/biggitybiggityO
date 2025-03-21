14eb20de15ebdf1c614247173d98b45ee94cf29f
komaksym <kovalmaksym2@gmail.com>
2025-03-21 01:49:23 +0100

    removed deepseek-coder-instr because, turned out, it was saved incorrectly and there were no weights? when trying inference, was getting 'tensors out of shape, expected: xxxx, got: 0 shape', but an instruction-tuned model likely wouldn't even be the optimal choice here

--- src/training/finetuning_deepseek-coder-6.7b-instruct/DeepSeek-Coder/finetune/config_gpu.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install gcc-10 g++-10 -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10  100 --slave /usr/bin/g++ g++ /usr/bin/g++-10
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run
-sudo sh cuda_11.7.0_515.43.04_linux.run
-export PATH=/usr/local/cuda-11.7/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64\
-                         {LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}

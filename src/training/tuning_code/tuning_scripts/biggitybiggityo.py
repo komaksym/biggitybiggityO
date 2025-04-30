0b4581e7a8bd519dc3af8d0a21a78199ab12cff9
komaksym <kovalmaksym2@gmail.com>
2025-04-30 03:02:52 +0200

    formatted the file

--- src/training/tuning_code/tuning_scripts/biggitybiggityo.py
+++ src/training/tuning_code/tuning_scripts/biggitybiggityo.py
@@ -1,79 +1,91 @@
 # Installs
-#pip install -U peft datasets bitsandbytes mlflow 
-#pip install flash-attn --no-build-isolation
+# pip install -U peft datasets bitsandbytes mlflow
+# pip install flash-attn --no-build-isolation
 
 # Imports
-import numpy as np
-import pdb
 import os
+import pdb
+from inspect import signature
+
+import numpy as np
 import torch
 import torch.nn as nn
-from inspect import signature
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, f1_score, make_scorer
-from datasets import load_dataset, Dataset
-from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification, \
-                        TrainingArguments, Trainer, DataCollatorWithPadding, BitsAndBytesConfig, \
-                         AutoConfig, PreTrainedModel, AutoModelForCausalLM
-from transformers.modeling_outputs import SequenceClassifierOutput
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from accelerate import PartialState
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from sklearn.metrics import accuracy_score, f1_score, make_scorer
+from sklearn.preprocessing import LabelEncoder
+from transformers import (
+    AutoConfig,
+    AutoModel,
+    AutoModelForCausalLM,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    DataCollatorWithPadding,
+    PreTrainedModel,
+    Trainer,
+    TrainingArguments,
+)
+from transformers.modeling_outputs import SequenceClassifierOutput
 
+from datasets import Dataset, load_dataset
 
 # Datasets
 DATASET_PATHS = {
     "local": {
         "train": "../../datasets/train_set.csv",
-        "test": "../../datasets/test_set.csv"
+        "test": "../../datasets/test_set.csv",
     },
-    "local_two": {
-        "train": "train_set.csv",
-        "test": "test_set.csv"
-    },
+    "local_two": {"train": "train_set.csv", "test": "test_set.csv"},
     "local_three": {
         "train": "drive/MyDrive/fine_tuning/train_set.csv",
-        "test": "drive/MyDrive/fine_tuning/test_set.csv"
+        "test": "drive/MyDrive/fine_tuning/test_set.csv",
     },
-
     "kaggle": {
         "train": "/kaggle/input/python-codes-time-complexity/train_set.csv",
-        "test": "/kaggle/input/python-codes-time-complexity/test_set.csv"
-    }
+        "test": "/kaggle/input/python-codes-time-complexity/test_set.csv",
+    },
 }
 
 
 def upload_datasets(dataset_paths=DATASET_PATHS):
     for path in dataset_paths:
-        if os.path.exists(dataset_paths[path]['train']) and os.path.exists(dataset_paths[path]['test']):
-            return dataset_paths[path]['train'], dataset_paths[path]['test']
+        if os.path.exists(dataset_paths[path]["train"]) and os.path.exists(
+            dataset_paths[path]["test"]
+        ):
+            return dataset_paths[path]["train"], dataset_paths[path]["test"]
 
-    return FileNotFoundError(f"Datasets do not exist in the current paths: {dataset_paths}")
+    return FileNotFoundError(
+        f"Datasets do not exist in the current paths: {dataset_paths}"
+    )
 
 
 train_set_path, test_set_path = upload_datasets()
 
 
 LABELS_HIERARCHY = {
-    'constant': 1,
-    'logn': 2,
-    'linear': 3,
-    'nlogn': 4,
-    'quadratic': 5,
-    'cubic': 6,
-    'np': 7
+    "constant": 1,
+    "logn": 2,
+    "linear": 3,
+    "nlogn": 4,
+    "quadratic": 5,
+    "cubic": 6,
+    "np": 7,
 }
 
 
 N_CLASSES = len(LABELS_HIERARCHY)
 
 
-train_set = load_dataset("csv", data_files=train_set_path)['train']
-eval_set = load_dataset("csv", data_files=test_set_path)['train']
+train_set = load_dataset("csv", data_files=train_set_path)["train"]
+eval_set = load_dataset("csv", data_files=test_set_path)["train"]
 
 
 # # Eval metrics
 def hc_score(y_true, y_pred, n_classes=N_CLASSES):
-    assert len(y_true) == len(y_pred), f"The amount of y_true labels: {len(y_true)} does not equal to the amount of y_pred: {len(y_pred)}."
+    assert len(y_true) == len(y_pred), (
+        f"The amount of y_true labels: {len(y_true)} does not equal to the amount of y_pred: {len(y_pred)}."
+    )
 
     n_samples = len(y_true)
 
@@ -82,35 +94,44 @@ def hc_score(y_true, y_pred, n_classes=N_CLASSES):
 
 def compute_metrics(eval_preds):
     logits, labels = eval_preds
-    preds = np.argmax(logits[0], axis=-1) if isinstance(logits, tuple) else np.argmax(logits, axis=-1)
+    preds = (
+        np.argmax(logits[0], axis=-1)
+        if isinstance(logits, tuple)
+        else np.argmax(logits, axis=-1)
+    )
 
     # Calculate accuracy
     accuracy = accuracy_score(labels, preds)
     # Calculate F-1 Macro
-    f1_macro_score = f1_score(labels, preds, average='macro')
+    f1_macro_score = f1_score(labels, preds, average="macro")
     # Calculate Hierarchy Score
     hierarchy_score = hc_score(labels, preds)
 
     return {
         "accuracy": accuracy,
         "f1_macro": f1_macro_score,
-        "hierarchy_score": hierarchy_score
+        "hierarchy_score": hierarchy_score,
     }
 
 
 # Tokenization
 # Setting up Label Encoder
 labelEncoder = LabelEncoder()
-labelEncoder.fit(train_set['complexity'])
+labelEncoder.fit(train_set["complexity"])
+
 
 def tokenize_data(data, tokenizer):
     # Tokenizing
-    tokenized = tokenizer(data['code'], truncation=True, max_length=512,)
-    tokenized['labels'] = labelEncoder.transform(data['complexity'])
+    tokenized = tokenizer(
+        data["code"],
+        truncation=True,
+        max_length=512,
+    )
+    tokenized["labels"] = labelEncoder.transform(data["complexity"])
     return tokenized
 
 
-def set_tokenizer(checkpoint): 
+def set_tokenizer(checkpoint):
     try:
         tokenizer = AutoTokenizer.from_pretrained(checkpoint, pad_token="<pad>")
     except Exception as e:
@@ -122,16 +143,16 @@ def set_tokenizer(checkpoint):
     X_train = train_set.map(
         lambda x: tokenize_data(x, tokenizer),
         batched=True,
-        remove_columns=train_set.column_names
+        remove_columns=train_set.column_names,
     )
     X_eval = eval_set.map(
         lambda x: tokenize_data(x, tokenizer),
         batched=True,
-        remove_columns=eval_set.column_names
+        remove_columns=eval_set.column_names,
     )
 
     # Data Collator
-    tokenizer.padding_side = 'left'
+    tokenizer.padding_side = "left"
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
     return tokenizer, data_collator, X_train, X_eval
 
@@ -140,10 +161,10 @@ def set_tokenizer(checkpoint):
 def setup_bnb_config():
     quant_config = BitsAndBytesConfig(
         load_in_4bit=True,
-        bnb_4bit_quant_type = 'nf4',
+        bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_storage=torch.bfloat16
+        bnb_4bit_quant_storage=torch.bfloat16,
     )
     return quant_config
 
@@ -154,11 +175,17 @@ def set_model(checkpoint, tokenizer, ModelType=AutoModel):
     quant_config = setup_bnb_config()
 
     # Load a pretrained model
-    model = ModelType.from_pretrained(checkpoint, torch_dtype=torch.bfloat16, num_labels=7, 
-                                     trust_remote_code=True, device_map=PartialState().process_index,
-                                     quantization_config=quant_config, attn_implementation="flash_attention_2")
+    model = ModelType.from_pretrained(
+        checkpoint,
+        torch_dtype=torch.bfloat16,
+        num_labels=7,
+        trust_remote_code=True,
+        device_map=PartialState().process_index,
+        quantization_config=quant_config,
+        attn_implementation="flash_attention_2",
+    )
 
-    # Accomodating the size of the token embeddings for the potential missing <pad> token 
+    # Accomodating the size of the token embeddings for the potential missing <pad> token
     model.resize_token_embeddings(len(tokenizer), mean_resizing=False)
 
     # Passing the pad token id to the model config
@@ -171,60 +198,77 @@ class DeepseekV2ForSequenceClassification(PreTrainedModel):
     config_class = AutoConfig
 
     def __init__(self, base_model, config):
-      super().__init__(config)
-      self.num_labels = config.num_labels
-      self.model = base_model
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.model = base_model
 
-      self.dense = nn.Linear(config.n_embd, config.num_labels, bias=False, dtype=self.model.dtype)
+        self.dense = nn.Linear(
+            config.n_embd, config.num_labels, bias=False, dtype=self.model.dtype
+        )
 
-      # Initialize weights and apply final processing
-      self.post_init()
-        
+        # Initialize weights and apply final processing
+        self.post_init()
+
     def get_input_embeddings(self):
         return self.model.config.n_embd
-        
-    def forward(self, input_ids=None, attention_mask=None, labels=None, *args, **kwargs):
-      outputs = self.model(input_ids, attention_mask)
 
-      hidden_states = outputs.last_hidden_state
-      logits = self.dense(hidden_states)
+    def forward(
+        self, input_ids=None, attention_mask=None, labels=None, *args, **kwargs
+    ):
+        outputs = self.model(input_ids, attention_mask)
 
-      # Batch size
-      if input_ids is not None:
-        batch_size = input_ids.shape[0]
+        hidden_states = outputs.last_hidden_state
+        logits = self.dense(hidden_states)
 
-      # If padding token id is not configured and the batch size is > 1
-      if self.config.pad_token_id is None and batch_size != 1:
-        raise ValueError("Cannot handle batch sizes > 1 if no padding token is defined.")
-      # If padding token id is not configured
-      if self.config.pad_token_id is None:
-        last_non_pad_token = -1
-      # if encoded inputs exist => find the last non padded token to pool data from
-      elif input_ids is not None:
-        non_pad_mask = (input_ids != self.config.pad_token_id).to(logits.device, dtype=torch.int32)
-        token_indices = torch.arange(input_ids.shape[-1], device=logits.device, dtype=torch.int32)
-        last_non_pad_token = (token_indices * non_pad_mask).argmax(-1)
+        # Batch size
+        if input_ids is not None:
+            batch_size = input_ids.shape[0]
 
-      # Pooling logits from the last non padded token across the batches
-      pooled_logits = logits[torch.arange(batch_size, device=logits.device), last_non_pad_token]
+        # If padding token id is not configured and the batch size is > 1
+        if self.config.pad_token_id is None and batch_size != 1:
+            raise ValueError(
+                "Cannot handle batch sizes > 1 if no padding token is defined."
+            )
+        # If padding token id is not configured
+        if self.config.pad_token_id is None:
+            last_non_pad_token = -1
+        # if encoded inputs exist => find the last non padded token to pool data from
+        elif input_ids is not None:
+            non_pad_mask = (input_ids != self.config.pad_token_id).to(
+                logits.device, dtype=torch.int32
+            )
+            token_indices = torch.arange(
+                input_ids.shape[-1], device=logits.device, dtype=torch.int32
+            )
+            last_non_pad_token = (token_indices * non_pad_mask).argmax(-1)
 
-      # Calculating loss if labels are provided
-      loss = None
-      if labels is not None:
-        loss = self.loss_function(logits=logits, labels=labels, pooled_logits=pooled_logits, config=self.config)
+        # Pooling logits from the last non padded token across the batches
+        pooled_logits = logits[
+            torch.arange(batch_size, device=logits.device), last_non_pad_token
+        ]
 
-      return SequenceClassifierOutput(loss=loss, logits=pooled_logits)
+        # Calculating loss if labels are provided
+        loss = None
+        if labels is not None:
+            loss = self.loss_function(
+                logits=logits,
+                labels=labels,
+                pooled_logits=pooled_logits,
+                config=self.config,
+            )
+
+        return SequenceClassifierOutput(loss=loss, logits=pooled_logits)
 
 
 # LoRA config
 peft_config = LoraConfig(
     r=16,
     lora_alpha=32,
-    #target_modules = ['q_proj', 'v_proj'], # Qwen 
-    target_modules = 'all-linear', # Heavy, universal
+    # target_modules = ['q_proj', 'v_proj'], # Qwen
+    target_modules="all-linear",  # Heavy, universal
     lora_dropout=0.1,
-    bias='none',
-    task_type = "SEQ_CLS" # might not work with this on
+    bias="none",
+    task_type="SEQ_CLS",  # might not work with this on
 )
 
 
@@ -232,24 +276,25 @@ peft_config = LoraConfig(
 
 
 def set_training_args(checkpoint, batch_size=16):
-    training_args = TrainingArguments(output_dir=f"training_results/{checkpoint}/",
-                                      eval_strategy="epoch",
-                                      save_strategy="epoch",
-                                      logging_strategy="epoch",
-                                      #eval_steps=5,
-                                      #learning_rate=2e-4, # Testing
-                                      bf16=True,
-                                      gradient_checkpointing=True,
-                                      report_to='mlflow',
-                                      num_train_epochs=3,
-                                      warmup_steps=100, # Testing
-                                      label_names=['labels'],
-                                      per_device_train_batch_size=batch_size,
-                                      per_device_eval_batch_size=batch_size,
-                                      gradient_accumulation_steps = 16,
-                                      load_best_model_at_end=True,
-                                      deepspeed="configs/ds_config.json",
-                                     )
+    training_args = TrainingArguments(
+        output_dir=f"training_results/{checkpoint}/",
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        logging_strategy="epoch",
+        # eval_steps=5,
+        # learning_rate=2e-4, # Testing
+        bf16=True,
+        gradient_checkpointing=True,
+        report_to="mlflow",
+        num_train_epochs=3,
+        warmup_steps=100,  # Testing
+        label_names=["labels"],
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        gradient_accumulation_steps=16,
+        load_best_model_at_end=True,
+        deepspeed="configs/ds_config.json",
+    )
     return training_args
 
 
@@ -260,16 +305,16 @@ checkpoint = "codellama/CodeLlama-70b-Python-hf"
 
 # Set up tokenizer, datasets, and model (as before)
 tokenizer, data_collator, train_set, eval_set = set_tokenizer(checkpoint)
-#train_set = train_set.select(range(100))
-#eval_set = eval_set.select(range(100))
+# train_set = train_set.select(range(100))
+# eval_set = eval_set.select(range(100))
 
 model = set_model(checkpoint, tokenizer, AutoModelForSequenceClassification)
-#model = DeepseekV2ForSequenceClassification(model, model.config)
+# model = DeepseekV2ForSequenceClassification(model, model.config)
 model = get_peft_model(model=model, peft_config=peft_config)
 
-#pdb.set_trace()
+# pdb.set_trace()
 print(f"Model: {model}")
-#print(f"Model config: {model.config}")
+# print(f"Model config: {model.config}")
 
 
 # Collecting
@@ -283,7 +328,7 @@ trainer = Trainer(
     eval_dataset=eval_set,
     data_collator=data_collator,
     processing_class=tokenizer,
-    compute_metrics=compute_metrics
+    compute_metrics=compute_metrics,
 )
 
 # Train

89500dba6a8d9f0748d5e34912e1e0c98edb39a7
komaksym <kovalmaksym2@gmail.com>
2025-05-22 23:49:31 +0200

    processed labels in the leetcode parsed dataset, and also converted neetcode and codecomplex datasets from .jsonl to .csv

--- datasets/preprocessing_scripts/format_labels.py
+++ datasets/preprocessing_scripts/format_labels.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from pathlib import Path
 
 
@@ -6,34 +7,50 @@ class LabelFormatter:
     def __init__(self, source_path, save_path=None):
         self.save_path = source_path if save_path is None else save_path
 
-        # Reading JSON file
-        with open(source_path, "r") as file:
-            self.data = [json.loads(line) for line in file]
+        self.source_dtype = str(source_path).split('.')[-1]
+        self.save_dtype = str(save_path).split('.')[-1]
+
+        # Read data
+        self.read_data(source_path)
 
         # Start formatting
         self.format()
+
         # Write changes
         self.save_formatted_json()
 
+    def read_data(self, source_path):
+        # Data in .json or .jsonl
+        if self.source_dtype == 'json' or self.source_dtype == 'jsonl':
+            with open(source_path, "r") as file:
+                self.data = [json.loads(line) for line in file]
+
+        # If data in .csv
+        elif self.source_dtype == 'csv':
+            self.data = pd.read_csv(source_path)
+
     def format_time_complexity(self, complexity):
         match complexity:
-            case "constant":
+            case "1":
                 return "O(1)"
 
             case "logn":
                 return "O(logn)"
 
-            case "linear":
+            case "n":
                 return "O(n)"
 
             case "nlogn":
                 return "O(nlogn)"
 
-            case "quadratic":
+            case "n^2":
                 return "O(n ^ 2)"
 
-            case "cubic":
+            case "n^3":
                 return "O(n ^ 3)"
+            
+            case "n!":
+                return "n!"
 
             case "np":
                 return "np"
@@ -42,22 +59,49 @@ class LabelFormatter:
                 return "other"
 
     def format(self):
-        for sample in self.data:
-            sample["complexity"] = self.format_time_complexity(
-                sample["complexity"]
-            )
+        if self.source_dtype == 'json' or self.source_dtype == 'jsonl':
+            for sample in self.data:
+                sample["label"] = self.format_time_complexity(
+                    sample["label"]
+                )
+        
+        elif self.source_dtype == 'csv':
+            self.data['label'] = self.data['label'].apply(self.format_time_complexity)
 
     def save_formatted_json(self):
-        with open(self.save_path, "w") as file:
-            for line in self.data:
-                file.write(json.dumps(line) + "\n")
+        match self.save_dtype:
+            # Save in .jsonl
+            case 'jsonl':
+                with open(self.save_path, "w") as file:
+                    for line in self.data:
+                        file.write(json.dumps(line) + "\n")
+
+            # Save in .json
+            case 'json':
+                with open(self.save_path, "w") as file:
+                    file.write(json.dumps(self.data))
+                
+            # Save in .csv
+            case 'csv':
+                """Before storing as .csv need to convert to a pd.DataFrame"""
+
+                # Source is in .jsonl, meaning reading by lines
+                if self.source_dtype == 'jsonl':
+                    self.data = pd.read_json(self.data, lines=True)
+
+                # Source is in .json, reading as a whole
+                elif self.source_dtype == 'json':
+                    self.data = pd.read_json(self.data, lines=True)
+
+                # Saving to .csv
+                self.data.to_csv(self.save_path, index=False)
 
 
 if __name__ == '__main__':
     BASE_PATH = Path(__file__).parent
 
-    source_path = BASE_PATH / "../data/codecomplex_data.jsonl"
-    save_path = BASE_PATH / "../data/codecomplex_data.jsonl"
+    source_path = BASE_PATH / "../data/clean_leetcode_data.csv"
+    save_path = BASE_PATH / "../data/clean_leetcode_data.csv"
 
     # Call
     foo = LabelFormatter(source_path, save_path)

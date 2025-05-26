91a5c388d41745b248c0b1a61fde953889bcc5da
komaksym <kovalmaksym2@gmail.com>
2025-05-26 15:18:16 +0200

    add relabeled audited clean leetcode data

---
+++ datasets/preprocessing_scripts/ai_relabel.py
@@ -0,0 +1,140 @@
+import os
+from pathlib import Path
+import pandas as pd
+from openai import AsyncOpenAI
+import asyncio
+from asyncio import Semaphore
+
+
+class LLM:
+    def __init__(self, client, model):
+        self.client = client
+        self.model = model
+        self.counter = 1
+
+    async def send_requests(self, sem, instructions, request, num_requests, **kwargs):
+        async with sem:
+            response = await self.client.chat.completions.create(
+                model=self.model,
+                **kwargs,
+                messages=[
+                    {"role": "system", "content": instructions},
+                    {"role": "user", "content": request},
+                ],
+                stream=False,
+            )
+            # Response
+            response = response.choices[0].message.content
+
+            # Log 
+            print(f"\nREQUEST (#{self.counter}/{num_requests}): {request}")
+            print("\nRESPONSE:\n", response, end='\n\n')
+
+            self.counter += 1
+
+            return response
+
+
+class Audit:
+    def __init__(self, source_path, llm, sem):
+        self.data_to_review = {"code": [], "complexity": [], "LLM_decision": []}
+        self.data = pd.read_csv(source_path, sep=",")
+
+        self.llm = llm
+        self.sem = sem
+
+    async def process_requests(self, instructions):
+        # Collect requests
+        requests = [
+            code + "\n\nComplexity: " + complexity
+            for code, complexity in zip(self.data["code"], self.data["complexity"])
+        ]
+
+        # Total # of data samples
+        num_requests = len(requests)
+
+        # Create tasks in the form of coroutine objects
+        tasks = [
+            self.llm.send_requests(
+                self.sem, instructions, request, num_requests,
+            )
+            for request in requests
+        ]
+
+        # Run coroutines concurrently
+        responses = await asyncio.gather(*tasks)
+
+        # Process responses
+        self.process_responses(responses)
+
+    def process_responses(self, responses):
+        for i, response in enumerate(responses):
+            # Add data under question to a separate dataset for further review
+            self.data_to_review["code"].append(self.data['code'].iloc[i])
+            self.data_to_review["complexity"].append(self.data['complexity'].iloc[i])
+            self.data_to_review['LLM_decision'].append(response)
+
+    def save_data_to_review(self, save_path):
+        # Save
+        df = pd.DataFrame(self.data_to_review)
+        df.to_csv(save_path, index=False)
+
+
+async def main():
+    # Client
+    client = AsyncOpenAI(
+        api_key=os.environ["XAI_API_KEY"],
+        base_url="https://api.x.ai/v1",
+    )
+    model = 'grok-3'
+
+    eval_instruction = """
+    You are a Python algorithms expert, specializing in mapping Python code to time complexity Big O labels.  
+    I will provide you with Python codes that are labeled with WORST-CASE big O time complexities. Your task is to evaluate the provided code sample and the mapped time complexity label, and output your own, evaluated, correct time complexity label.  
+    Change the original label only if it is incorrect, and the difference between the specified time complexity label and the real label is big, for example, if the time complexity might be O(1), and in some cases O(n), and the provided label is in one of the possible labels, then do NOT change it, or if the specified time complexity label is correct but doesn't include a constant, omit it and do NOT change it as the difference is insignificant compared to if the specified label was n, but the actual label was logn or nlogn or n^2.  
+
+    Your response shouldn't contain anything but a label. One word. 
+
+    Example input:
+        "class Solution(object):
+            def minimumLines(self, stockPrices):
+                def gcd(a, b):
+                    while b:
+                        a, b = b, a%b
+                    return a
+            
+                stockPrices.sort()
+                result = 0
+                prev = None
+                for i in range(1, len(stockPrices)):
+                    dy, dx = stockPrices[i][1]-stockPrices[i-1][1], stockPrices[i][0]-stockPrices[i-1][0]
+                    g = gcd(dy, dx)
+                    if not prev or prev != (dy//g, dx//g):
+                        prev = (dy//g, dx//g)
+                        result += 1
+                return result"	
+
+                Complexity: O(nlogn)
+
+    Example response:  
+        O(nlogn)
+"""
+
+    BASE_PATH = Path(__file__).parent
+    source_path = BASE_PATH / "../data/ai_audited/audited_clean_leetcode_data.csv"
+    save_path = BASE_PATH / "../data/ai_audited/relabeled_audited_clean_leetcode_data.csv"
+
+    # LLMs to use
+    grok = LLM(client, model)
+    semaphore = Semaphore(5)
+
+    # Initiate audit
+    audit = Audit(source_path, grok, semaphore)
+    # Send requests
+    await audit.process_requests(eval_instruction)
+    # Save data for review
+    audit.save_data_to_review(save_path)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

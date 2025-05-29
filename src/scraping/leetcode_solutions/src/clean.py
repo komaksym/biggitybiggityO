a909c8f52963ed7362898afe8dade1ec53429adf
komaksym <kovalmaksym2@gmail.com>
2025-05-29 16:22:44 +0200

    refactor clean.py from leetcode data parsing scripts

--- src/scraping/leetcode_solutions/src/clean.py
+++ src/scraping/leetcode_solutions/src/clean.py
@@ -1,127 +1,131 @@
 import re
-from .parse import search_files
-import subprocess
-import pandas as pd
 from pathlib import Path
 
+import pandas as pd
 
-# Defie a cleaning pattern
-def set_regex_pattern(pattern, flags=0):
-    return re.compile(rf"{pattern}", flags)
+from utils import open_corrupted_files, search_files, set_regex_pattern
+
+BASE_LOCATION = Path(__file__).parent
 
 
-# Clean raw data and write
-def clean_files(files, file_paths, filter_pttrn):
-    parsed_data = {'code': [], 'label': []}
-    corrupted_data = []
-    num_of_duplicates = 0
+class FilesCleaner:
+    def __init__(self):
+        self.parsed_data = {"code": [], "label": []}
+        self.corrupted_data = []
+        self.num_of_duplicates = 0
 
-    for file, file_path in zip(files, file_paths):
-        lines = file.splitlines()
+    def clean(self, files, file_paths, regex_pattern):
+        """Iterate files line by line and duplicate labels for solutions 
+           that do not have its own label by taking the predecessor.
 
-        label = []
-        code = []
-        i = 0
-        next_label_exists = True
+           The idea is for every solution to have its own label which will get
+           the files into a common format that we can then parse.
 
-        while i < len(lines):
-            # Keep capturing while complexity comments are present
-            while i < len(lines) and re.search(r"#.*\sO\(", lines[i]):
-                label.append(lines[i])
-                i += 1
+           We also save 'corrupted' data, where there was not even a single label
+           to borrow for solutions.
+           """
+        for file, file_path in zip(files, file_paths):
+            lines = file.splitlines()
 
-            # Add the label only once before a solution is added
-            if label and len(parsed_data['label']) - len(parsed_data['code']) < 1:
-                parsed_data['label'].append('\n'.join(label))
-            
-            # Check if it's the definition of solution
-            if i < len(lines) and re.search(r"(?:class|def)\sSolution", lines[i], flags=re.IGNORECASE):
+            label = []
+            code = []
+            i = 0
+            next_label_exists = True
 
-                # Keep consuming until a label from the next solution is hit
-                while i < len(lines) and not re.search(r"#.*\sO\(", lines[i]):
-                    code.append(lines[i])
-                    next_label_exists = True
+            while i < len(lines):
+                # Keep capturing while complexity comments are present
+                while i < len(lines) and re.search(r"#.*\sO\(", lines[i]):
+                    label.append(lines[i])
                     i += 1
 
-                    # If no next label was found (from the next solution)
-                    if i < len(lines) and re.search(r"(?:class|def)\sSolution", lines[i], flags=re.IGNORECASE):
-                        num_of_duplicates += 1
-                        next_label_exists = False
-                        # New solution was found, break out of the loop
-                        break
+                # Add the label only once before a solution is added
+                if (
+                    label
+                    and len(self.parsed_data["label"]) - len(self.parsed_data["code"])
+                    < 1
+                ):
+                    self.parsed_data["label"].append("\n".join(label))
 
-                # Add the code snippet
-                solution = re.sub(filter_pttrn, "", '\n'.join(code))
-                parsed_data['code'].append(solution)
+                # Check if it's the definition of solution
+                if i < len(lines) and re.search(
+                    r"(?:class|def)\sSolution", lines[i], flags=re.IGNORECASE
+                ):
+                    # Keep consuming until a label from the next solution is hit
+                    while i < len(lines) and not re.search(r"#.*\sO\(", lines[i]):
+                        code.append(lines[i])
+                        next_label_exists = True
+                        i += 1
 
-                # If there was no label at the beginning, add a placeholder
-                if len(parsed_data['code']) > len(parsed_data['label']):
-                    parsed_data['label'].append('')
-                
-                # Reset the current label only if the next one exists, else keep it for further borrowing 
-                if next_label_exists:
-                    label = []
+                        # If no next label was found (from the next solution)
+                        if i < len(lines) and re.search(
+                            r"(?:class|def)\sSolution", lines[i], flags=re.IGNORECASE
+                        ):
+                            self.num_of_duplicates += 1
+                            next_label_exists = False
+                            # New solution was found, break out of the loop
+                            break
 
-                # Reset the current label and code snippet
-                code = []
+                    # Add the code snippet
+                    solution = re.sub(regex_pattern, "", "\n".join(code))
+                    self.parsed_data["code"].append(solution)
 
-                # Rewind one step back to account for the main iteration
-                i -= 1
+                    # If there was no label at the beginning, add a placeholder
+                    if len(self.parsed_data["code"]) > len(self.parsed_data["label"]):
+                        self.parsed_data["label"].append("")
 
-            # Next line
-            i += 1
+                    # Reset the current label only if the next one exists, else keep it for further borrowing
+                    if next_label_exists:
+                        label = []
 
-        # Make sure we have the same number of solutions as labels
-        if len(parsed_data['label']) != len(parsed_data['code']):
-            corrupted_data.append(file_path)
-            
-    print(f"Number of duplicates: {num_of_duplicates}.")
+                    # Reset the current label and code snippet
+                    code = []
 
-    return parsed_data, corrupted_data
+                    # Rewind one step back to account for the main iteration
+                    i -= 1
+
+                # Next line
+                i += 1
+
+            # Make sure we have the same number of solutions as labels
+            if len(self.parsed_data["label"]) != len(self.parsed_data["code"]):
+                self.corrupted_data.append(file_path)
+
+        print(f"Number of duplicates: {self.num_of_duplicates}.")
+
+        return self.parsed_data, self.corrupted_data
+
+    def save(self, data_to_save, path):
+        if not str(path).endswith(".csv"):
+            raise ValueError("Make sure the type of the file to save is '.csv'")
+
+        df = pd.DataFrame(data_to_save)
+        df.to_csv(path, index=False)
+
+        print(f"Successfully saved {len(data_to_save['code'])} solutions.")
 
 
-def open_corrupted_files(command, posix_paths, destination_path=None):
-    # Subprocess command
-    command = command
-    paths = [command]
-
-    # Populate the paths
-    for p in posix_paths:
-    # Get the str representation of PosixPath
-        paths.append(str(p))
-
-    # If moving files
-    if command == 'mv' and destination_path:
-        paths.append(destination_path)
-
-    # Run
-    if posix_paths:
-        subprocess.run(paths)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Absolute path to script's directory
-    BASE_LOCATION = Path(__file__).parent
-    files_path =  BASE_LOCATION.parent / 'data/raw_files/messy_files/unclear_files'
-    print("Searching files...")
+    fp_source = BASE_LOCATION.parent / "data/raw_files/messy_files/annotated_files"
+
     # Read raw data
-    raw_data = search_files(files_path)
-    print(f"Successfully found {len(raw_data['files'])} files.")
+    raw_data = search_files(fp_source)
 
     # Regex pattern
-    FILTER_PATTERN = set_regex_pattern("(#.*?$)|(\"{3}.*?\"{3})|('{3}.*?'{3})", flags=re.DOTALL | re.IGNORECASE | re.MULTILINE)
+    filter_pattern = set_regex_pattern(
+        "(#.*?$)|(\"{3}.*?\"{3})|('{3}.*?'{3})",
+        flags=re.DOTALL | re.IGNORECASE | re.MULTILINE,
+    )
 
-    print("Cleaning the files...")
     # Clean the data up
-    parsed_data, corrupted_data = clean_files(raw_data['files'], raw_data['file_paths'], FILTER_PATTERN)
-
-    print("Successfully finished cleaning the data.")
-    print(f"Unsuccessfully cleaned {len(corrupted_data)} files.")
+    cleaner = FilesCleaner()
+    parsed_data, corrupted_data = cleaner.clean(
+        **raw_data, regex_pattern=filter_pattern
+    )
 
     # Inspect corrupted files
-    open_corrupted_files('open', corrupted_data)
+    open_corrupted_files("open", corrupted_data)
 
-    df = pd.DataFrame(parsed_data)
-    df.to_csv("messy_data.csv", index=False)
-
-    print(f"Successfully saved {len(parsed_data['code'])} solutions.")
+    # Save parsed data
+    save_path = BASE_LOCATION.parent / "test.csv"
+    cleaner.save(parsed_data, save_path)
---
+++ src/scraping/leetcode_solutions/src/utils.py
@@ -0,0 +1,35 @@
+import re
+import subprocess
+
+
+def set_regex_pattern(pattern, flags=0):
+    return re.compile(rf"{pattern}", flags)
+
+
+def search_files(folder_path):
+    raw_data = {"file_paths": [], "files": []}
+
+    for file_path in folder_path.glob("*.py"):
+        raw_data["file_paths"].append(file_path)
+        raw_data["files"].append(file_path.read_text())
+
+    return raw_data
+
+
+def open_corrupted_files(command, posix_paths, destination_path=None):
+    # Subprocess command
+    command = command
+    paths = [command]
+
+    # Populate the paths
+    for p in posix_paths:
+        # Get the str representation of PosixPath
+        paths.append(str(p))
+
+    # If moving files
+    if command == "mv" and destination_path:
+        paths.append(destination_path)
+
+    # Run
+    if posix_paths:
+        subprocess.run(paths)

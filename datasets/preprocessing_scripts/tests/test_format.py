4b45d1bed00b1067afd0e469f759f60df193e321
komaksym <kovalmaksym2@gmail.com>
2025-06-03 01:43:00 +0200

    add mock data as temp files instead of local ones

--- datasets/preprocessing_scripts/tests/test_format.py
+++ datasets/preprocessing_scripts/tests/test_format.py
@@ -1,5 +1,5 @@
 from ..format import format_data
-from ..utils import read_data
+from ..utils import save_data
 from pathlib import Path
 import pytest
 import pandas as pd
@@ -8,13 +8,20 @@ import pandas as pd
 BASE_LOCATION: Path = Path(__file__).parent
 
 
-@pytest.fixture
-def dummy_source_path() -> Path:
-    return BASE_LOCATION / "dummy_data/dummy_src_data.csv"
+@pytest.fixture(scope="package")
+def dummy_dataset(tmp_path_factory):
+    """Create a temp dataset"""
+    tmp_data = pd.DataFrame({"code": ["class Solution(object):\n    def findEvenNumbers(self, digits):\n        k = 3\n        def backtracking(curr, cnt, result): \n# Testing testing 123\n\n            if len(curr) == k:\n                result.append(reduce(lambda x, y: x*10+y, curr))\n                return\n            for i, c in enumerate(cnt):\n                if c == 0 or (not curr and i == 0) or (len(curr) == k-1 and i%2 != 0):\n                    continue\n                cnt[i] -= 1\n                curr.append(i)\n                backtracking(curr, cnt, result)\n                curr.pop()\n                cnt[i] += 1\n\n        cnt = [0]*10\n        for d in digits:\n            cnt[d] += 1\n        result = []\n        backtracking([], cnt, result)\n        return result", "class Solution2(object):\n    def findEvenNumbers(self, digits):\n\n\n\n\n\n\n\n        result, cnt = [], collections.Counter(digits)\n        for i in range(1, 10):\n            for j in range(10):\n                for k in range(0, 10, 2):\n                    if cnt[i] > 0 and cnt[j] > (j == i) and cnt[k] > (k == i) + (k == j):\n                        result.append(i*100 + j*10 + k)\n        return result", "class Solution3(object):\n    def findEvenNumbers(self, digits):\n        k = 3\n        \n        def backtracking(curr, dummy, result):\n            if len(curr) == k:\n                result.append(reduce(lambda x, y: x*10+y, curr))\n                return\n            node = dummy.right\n            while node:\n                if (not curr and node.val[0] == 0) or (len(curr) == k-1 and node.val[0]%2 != 0):\n                    node = node.right\n                    continue\n                node.val[1] -= 1\n                if node.val[1] == 0:\n                    if node.left:\n                        node.left.right = node.right\n                    if node.right:\n                        node.right.left = node.left\n                curr.append(node.val[0])\n                backtracking(curr, dummy, result)\n                curr.pop()\n                if node.val[1] == 0:\n                    if node.left:\n                        node.left.right = node\n                    if node.right:\n                        node.right.left = node\n                node.val[1] += 1\n                node = node.right\n\n        prev = dummy = Node()\n        for digit, cnt in sorted(map(list, iter(collections.Counter(digits).items()))):\n            prev.right = Node(val=[digit, cnt], left=prev)\n            prev = prev.right\n        result = []\n        backtracking([], dummy, result)\n        return result"],
+                             "complexity": ['O(n)', '# Time:  O(n), n is 10^3', '# Time:  O(1) ~ O(n), n is 10^3']})
+    file_path = tmp_path_factory.mktemp("dummy_data") / "df.csv"
+    save_data(tmp_data, file_path)
+    return tmp_data
+    
 
 
 @pytest.fixture
 def expected_formatted_data():
+    """Expected data post-format"""
     return pd.DataFrame({
         "code": [
             "class Solution(object):\n    def findEvenNumbers(self, digits):\n        k = 3\n        def backtracking(curr, cnt, result):\n\n            if len(curr) == k:\n                result.append(reduce(lambda x, y: x*10+y, curr))\n                return\n            for i, c in enumerate(cnt):\n                if c == 0 or (not curr and i == 0) or (len(curr) == k-1 and i%2 != 0):\n                    continue\n                cnt[i] -= 1\n                curr.append(i)\n                backtracking(curr, cnt, result)\n                curr.pop()\n                cnt[i] += 1\n\n        cnt = [0]*10\n        for d in digits:\n            cnt[d] += 1\n        result = []\n        backtracking([], cnt, result)\n        return result"
@@ -23,7 +30,7 @@ def expected_formatted_data():
     })
 
 
-def test_format_data(dummy_source_path, expected_formatted_data):
-    got = format_data(read_data(dummy_source_path))
+def test_format_data(dummy_dataset, expected_formatted_data):
+    got = format_data(dummy_dataset)
 
     assert got.equals(expected_formatted_data)

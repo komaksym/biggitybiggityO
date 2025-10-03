00c921f131bbf0a9743a7dc67e29496d6ddddb96
komaksym <kovalmaksym2@gmail.com>
2025-10-03 12:31:54 +0000

    add run training script command

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ git config --global user.email "kovalmaksym2@gmail.com"
 git config --global user.name "komaksym"
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+accelerate launch --config_file src/training/code/scripts/configs/fsdp_config.yaml src/training/code/scripts/train.py

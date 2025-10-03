319c4f26997fad2c6c33de4f51287a365530baef
komaksym <kovalmaksym2@gmail.com>
2025-10-03 11:49:05 +0200

    change git setup and download oh-my-bash places

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 source bin/activate
 pip install -r requirements.txt
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
 git remote set-url origin  git@github.com:komaksym/biggitybiggityO.git
 git config --global user.email "kovalmaksym2@gmail.com"
 git config --global user.name "komaksym"
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"

7f84c7249b39774be8a681edc35ea896d9e4e4ca
komaksym <kovalmaksym2@gmail.com>
2025-10-03 12:33:41 +0000

    add comments

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Activate venv and install packages
 source bin/activate
 pip install -r requirements.txt
 
+# Set up git to be able to push to github
 git remote set-url origin  git@github.com:komaksym/biggitybiggityO.git
 git config --global user.email "kovalmaksym2@gmail.com"
 git config --global user.name "komaksym"
 
+# Install oh-my-bash for ga gc gd commands
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
+# Training script
 accelerate launch --config_file src/training/code/scripts/configs/fsdp_config.yaml src/training/code/scripts/train.py

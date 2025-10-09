6bb81a7289b1ccfce3b4193351f2d9e0766c60dc
komaksym <kovalmaksym2@gmail.com>
2025-10-09 12:40:04 +0000

    prep for test tuning Qwen 3 coder 30b

--- src/training/code/scripts/configs/config.py
+++ src/training/code/scripts/configs/config.py
@@ -1,8 +1,8 @@
 import torch
 from transformers import TrainingArguments
 
-checkpoint = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
-batch_size = 8
+checkpoint = "Qwen/Qwen3-Coder-30B-A3B-Instruct"
+batch_size = 1
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Training args
@@ -16,12 +16,12 @@ training_args = TrainingArguments(
     bf16=True,
     # gradient_checkpointing=True,
     #report_to="mlflow",
-    num_train_epochs=3,
+    num_train_epochs=1,
     # warmup_steps=100,  # Testing
     label_names=["labels"],
     per_device_train_batch_size=batch_size,
     per_device_eval_batch_size=batch_size,
-    gradient_accumulation_steps=2,
+    gradient_accumulation_steps=16,
     load_best_model_at_end=True,
     run_name=f"{checkpoint}".split("/")[-1],
     # deepspeed="configs/ds_config.json",
--- src/training/code/scripts/data.py
+++ src/training/code/scripts/data.py
@@ -60,8 +60,8 @@ train_set = train_set.apply(generate_prompt, axis=1)
 eval_set = eval_set.apply(generate_prompt, axis=1)
 
 # Fractionize for faster testing iterations
-#train_set = train_set.sample(frac=0.01)
-#eval_set = eval_set.sample(frac=0.01)
+train_set = train_set.sample(frac=0.01)
+eval_set = eval_set.sample(frac=0.01)
 
 # Load as huggingface Datasets
 train_set = Dataset.from_pandas(train_set)

284d4437cd29c6027c3c974c56fe9d4ed772cef5
komaksym <kovalmaksym2@gmail.com>
2025-10-11 22:48:03 +0200

    add custom loss - focal loss

--- src/training/code/scripts/train.py
+++ src/training/code/scripts/train.py
@@ -1,14 +1,46 @@
-from configs.config import training_args, checkpoint
+import torch
+import torch.nn.functional as F
+from configs.config import checkpoint, training_args
 from data import data_collator, eval_set, tokenizer, train_set
-from model import model
 from evaluate import ConfusionMatrixCallback, RecallScoreCallback, compute_metrics
+from model import model
+from transformers import Trainer
 from utils import setup_mlflow
 
-from transformers import Trainer
+
+class CustomLossTrainer(Trainer):
+    def __init__(self, loss_fn=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Store custom loss here
+        # Should take (logits, labels) as args
+        self.loss_fn = loss_fn
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        # Assume labels is the name of the y ground truth
+        labels = inputs.get("labels")
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+
+        # Compute custom loss using our function
+        loss = self.loss_fn(logits, labels)
+
+        return (loss, outputs) if return_outputs else loss
+
+
+# Implement Focal Loss
+def focal_loss(logits, labels, gamma=2.0, alpha=0.25):
+    # Compute cross entropy loss first
+    ce_loss = F.cross_entropy(logits, labels, reduction=None)
+    # Get probs
+    probs = torch.exp(-ce_loss)
+    # Compute focal loss
+    focal_loss = alpha * (1 - probs) ** gamma * ce_loss
+
+    return focal_loss
 
 
 # Building
-trainer = Trainer(
+trainer = CustomLossTrainer(
     model=model,
     args=training_args,
     train_dataset=train_set,
@@ -16,6 +48,7 @@ trainer = Trainer(
     data_collator=data_collator,
     processing_class=tokenizer,
     compute_metrics=compute_metrics,
+    loss_fn=focal_loss,
     callbacks=[ConfusionMatrixCallback(), RecallScoreCallback()],
 )
 
@@ -39,7 +72,6 @@ def main():
 
     trainer.save_model(f"./best_model/{checkpoint}/")
     print("The best model was saved.")
-
 
 
 if __name__ == "__main__":

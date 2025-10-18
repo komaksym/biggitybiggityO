dbd908230b5817df9cdd8860ce1759a5b05bf942
komaksym <kovalmaksym2@gmail.com>
2025-10-18 12:31:28 +0000

    fix predict

--- src/training/code/scripts/predict.py
+++ src/training/code/scripts/predict.py
@@ -1,43 +1,61 @@
-from data import tokenizer
-from datasts import Dataset
-from train import trainer
+import torch
+from datasets import Dataset
 from data import labelEncoder
 import numpy as np
-from model import model
+from model import base_model
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from pathlib import Path
+from peft import PeftModel
+from configs.config import device
 
+BASE_LOCATION: Path = Path(__file__).parent
 
-def predict(inputs):
+def predict(inputs, model, tokenizer):
     # Tokenizing inputs
-    test_sample = tokenizer(inputs, return_tensors="pt", padding=True, truncation=True)
-    inputs = Dataset.from_dict({key: value.to(model.device) for key, value in test_sample.items()})
+    inputs = tokenizer(inputs, return_tensors="pt", padding=True, truncation=True).to(device=model.device)
 
     # Predicting & decoding inputs
-    preds = trainer.predict(test_dataset=inputs)
-    preds = labelEncoder.inverse_transform(y=np.ravel(np.argmax(preds.predictions[0], axis=-1)))
+    preds = model(**inputs)
+    logits = preds.logits[0].to(dtype=torch.float32).cpu().detach().numpy()
+    preds = labelEncoder.inverse_transform(y=np.ravel(np.argmax(logits, axis=-1)))
 
-    return preds
+    return preds[0]
 
 
 def main():
     test_sample = """
-    class Solution:
-        def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-            count = {}
-            for num in nums:
-                count[num] = 1 + count.get(num, 0)
+ def Onlogn_merge_sort(sequence):
+    if len(sequence) < 2:
+        return sequence
+    
+    m = len(sequence) // 2
+    return Onlogn_merge(Onlogn_merge_sort(sequence[:m]), Onlogn_merge_sort(sequence[m:]))
 
-            arr = []
-            for num, cnt in count.items():
-                arr.append([cnt, num])
-            arr.sort()
 
-            res = []
-            while len(res) < k:
-                res.append(arr.pop()[1])
-            return res
+def Onlogn_merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+
+    return result
             """
+    
+    pretrained_path = BASE_LOCATION.parents[3] / "best_model/deepseek-ai/deepseek-coder-1.3b-base/"
 
-    predict(test_sample)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_path)
+    model = PeftModel.from_pretrained(base_model, pretrained_path, dtype="auto", device_map="auto")
+
+    response = predict(test_sample, model, tokenizer)
+    print(f"LABEL: {response}")
+
 
 
 if __name__ == "__main__":

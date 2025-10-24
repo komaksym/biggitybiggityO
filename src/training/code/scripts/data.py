e0491b6e8da89af4f92e50b9bfa456b3efc2715e
komaksym <kovalmaksym2@gmail.com>
2025-10-24 18:03:31 +0000

    fix label2id

--- src/training/code/scripts/data.py
+++ src/training/code/scripts/data.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
 
-from configs.config import checkpoint
+import numpy as np
+import torch
+from configs.config import checkpoint, device
 from sklearn.preprocessing import LabelEncoder
 from transformers import AutoTokenizer, DataCollatorWithPadding
 import pandas as pd
@@ -64,8 +66,8 @@ train_set = train_set.apply(generate_prompt, axis=1)
 eval_set = eval_set.apply(generate_prompt, axis=1)
 
 # Fractionize for faster testing iterations
-train_set = train_set.sample(frac=0.1)
-eval_set = eval_set.sample(frac=0.1)
+#train_set = train_set.sample(frac=0.1)
+#eval_set = eval_set.sample(frac=0.1)
 
 # Load as huggingface Datasets
 train_set = Dataset.from_pandas(train_set)
@@ -73,12 +75,9 @@ eval_set = Dataset.from_pandas(eval_set)
 
 # Encoding labels
 # Specifying the order of the labels
-label2id = {'O(1)': 1, 'O(logn)': 2, 'O(n)': 3, 'O(nlogn)': 4, 'O(n ^ 2)': 5, 'O(n ^ 3)': 6, 'np': 7}
+label2id = {'O(1)': 0, 'O(logn)': 1, 'O(n)': 2, 'O(nlogn)': 3, 'O(n ^ 2)': 4, 'O(n ^ 3)': 5, 'np': 6}
 id2label = {label2id[k]: k for k in label2id.keys()}
 
-def encode_labels(val):
-    """Applier function for encoding labels"""
-    return label2id[val]
 
 # Tokenization
 def tokenize_data(data, tokenizer):
@@ -87,7 +86,7 @@ def tokenize_data(data, tokenizer):
         truncation=True,
         max_length=512,
     )
-    tokenized["labels"] = data["complexity"].apply(encode_labels) # Not sure if needed for the prompt schema
+    tokenized["labels"] = np.array([label2id[label] for label in data["complexity"]]) # Not sure if needed for the prompt schema
     return tokenized
 
 

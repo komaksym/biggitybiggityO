6da68180189eac07e8d2c7553dccb754ffe72a31
komaksym <kovalmaksym2@gmail.com>
2025-10-27 09:55:32 +0000

    fix display labels

--- src/training/code/scripts/evaluate.py
+++ src/training/code/scripts/evaluate.py
@@ -73,8 +73,6 @@ class ConfusionMatrixCallback(TrainerCallback):
                     "O(nlogn)",
                     "O(n^2)",
                     "O(n^3)",
-                    "O(2^n)",
-                    "O(n!)",
                     "np",
                 ],
             )

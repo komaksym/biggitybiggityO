684753eb4afecf32d1573a65ea6b9c017ed78445
komaksym <kovalmaksym2@gmail.com>
2025-10-29 20:53:37 +0100

    remove the installation of oh-my-bash

--- setup.sh
+++ setup.sh
@@ -8,6 +8,3 @@ pip install -r requirements.txt
 git remote set-url origin  git@github.com:komaksym/biggitybiggityO.git
 git config --global user.email "kovalmaksym2@gmail.com"
 git config --global user.name "komaksym"
-
-# Install oh-my-bash for ga gc gd commands
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"

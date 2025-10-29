cbef862758f06ecc87daec430e47b84c0206b009
komaksym <kovalmaksym2@gmail.com>
2025-10-29 18:07:27 +0100

    add minor formatting and remove custom prediction func, as we're using trainer to eval on test set

--- src/training/code/scripts/predict.py
+++ src/training/code/scripts/predict.py
@@ -1,7 +1,5 @@
-import torch
-from data import id2label, generate_prompt, tokenize_data
+from data import tokenize_data, generate_prompt
 from evaluate import compute_metrics
-import numpy as np
 from model import base_model
 from transformers import AutoTokenizer, Trainer
 from pathlib import Path
@@ -11,45 +9,36 @@ from datasets import Dataset
 
 BASE_LOCATION: Path = Path(__file__).parent
 
-def predict(inputs, model):
-    # Predicting & decoding inputs
-    preds = model(**torch.tensor(inputs.to_dict()))
-    logits = preds.logits[0].to(dtype=torch.float32).cpu().detach().numpy()
-    prediction = np.ravel(np.argmax(logits, axis=-1))[0]
-    preds = id2label[prediction]
-
-    return preds
-
 
 def main():
+    # Path for the pretrained model
     pretrained_path = BASE_LOCATION.parents[1] / "models/best_model/deepseek-ai/deepseek-coder-1.3b-base/"
 
+    # Loading pretrained tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained(pretrained_path)
     model = PeftModel.from_pretrained(base_model, pretrained_path, dtype="auto", device_map="auto")
+
     # Read the test set
     test_set = pd.read_csv("test_set.csv")
     # Apply the prompt schema
-    #test_set = test_set.apply(generate_prompt, axis=1)
+    test_set = test_set.apply(generate_prompt, axis=1)
+    # Convert to Dataset
     test_set = Dataset.from_pandas(test_set)
 
     # Tokenize
     test_set = test_set.map(
-        lambda x: tokenize_data(x, tokenizer),
-        batched=True,
-        remove_columns=test_set.column_names
-    )
-    
-    trainer = Trainer(
-        model=model, 
-        processing_class=tokenizer,
-        eval_dataset=test_set,
-        compute_metrics=compute_metrics
+        lambda x: tokenize_data(x, tokenizer), batched=True, remove_columns=test_set.column_names
     )
 
+    # Init Trainer
+    trainer = Trainer(
+        model=model, processing_class=tokenizer, eval_dataset=test_set, compute_metrics=compute_metrics
+    )
+
+    # Evaluate and save results
     results = trainer.evaluate()
 
     print(results)
-
 
 
 if __name__ == "__main__":

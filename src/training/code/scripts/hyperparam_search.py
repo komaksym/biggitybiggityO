a8cd9a662c3b3875db5ac748883988e2f8e3cf8e
komaksym <kovalmaksym2@gmail.com>
2025-10-30 21:32:59 +0100

    minor refactoring

--- src/training/code/scripts/hyperparam_search.py
+++ src/training/code/scripts/hyperparam_search.py
@@ -18,16 +18,16 @@ from transformers import (
     TrainingArguments,
 )
 
-from .configs.config import DATASET_PATHS
-from .data import label2id, set_tokenizer
-from .model import DeepseekV2ForSequenceClassification
-from .train import load_data, preprocess_data, setup_model
+from configs.config import DATASET_PATHS
+from data import label2id, set_tokenizer
+from model import DeepseekV2ForSequenceClassification, set_model
+from train import load_data, preprocess_data, setup_model
 
 # Model
-checkpoint = "deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"
+checkpoint = "deepseek-ai/deepseek-coder-1.3b-base"
 
 # Init wandb for tracking
-wandb.init(project="HPS-optuna", name="hyperparameter_search_optuna")
+#wandb.init(project="HPS-optuna", name="hyperparameter_search_optuna")
 
 # Define persistent storage
 storage = RDBStorage("sqlite:///optuna_trials_deepseek-coder-v2.db")
@@ -49,31 +49,11 @@ def objective(trial):
     batch_size = 4
     gradient_accumulation_steps_ = hps_batch_size // batch_size
 
-    # Bitsandbytes (Quantization)
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_storage=torch.bfloat16,
-    )
-
     # Load the pretrained model
-    base_model = AutoModel.from_pretrained(
-        checkpoint,
-        torch_dtype="auto",
-        num_labels=N_CLASSES,
-        trust_remote_code=True,
-        device_map="auto",
-        attn_implementation="flash_attention_2",  # Only for newer models
-        quantization_config=bnb_config,
-    )
+    base_model = set_model(checkpoint, tokenizer)
 
-    # Accomodating the size of the token embeddings for the potential missing <pad> token
-    base_model.resize_token_embeddings(len(tokenizer), mean_resizing=False)
-
-    # Passing the pad token id to the model config
-    base_model.config.pad_token_id = tokenizer.pad_token_id
+    # Apply custom classification head for Deepseek v2 architecture
+    #base_model = DeepseekV2ForSequenceClassification(base_model, base_model.config)
 
     # LoRA config
     peft_config = LoraConfig(
@@ -85,9 +65,6 @@ def objective(trial):
         bias="none",
         task_type="SEQ_CLS",  # might not work with this on
     )
-
-    # Apply custom classification head for Deepseek v2 architecture
-    base_model = DeepseekV2ForSequenceClassification(base_model, base_model.config)
 
     # LoRA
     peft_model = get_peft_model(model=base_model, peft_config=peft_config)
@@ -137,7 +114,9 @@ if __name__ == "__main__":
     tokenizer, data_collator = set_tokenizer(checkpoint)
 
     # Prep the data
-    train_set, eval_set = preprocess_data(load_data(DATASET_PATHS), tokenizer, label2id)
+    train_set, eval_set = load_data(DATASET_PATHS)
+    # Preprocess the data
+    train_set, eval_set = preprocess_data(train_set, eval_set, tokenizer, label2id)
 
     # Setup model
     model = setup_model(tokenizer, checkpoint)

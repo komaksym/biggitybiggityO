0d160d694f4bbab6b5490b654c148ce07f5790ee
komaksym <kovalmaksym2@gmail.com>
2025-10-30 21:34:50 +0100

    minor formatting

--- tests/training/data/test_data.py
+++ tests/training/data/test_data.py
@@ -2,17 +2,23 @@ import great_expectations as gx
 import pandas as pd
 import pytest
 from pathlib import Path
-from pprint import pprint
+from src.training.code.scripts.data import find_paths, generate_prompt
 
-BASE_PATH = Path(__file__).parent
+BASE_LOCATION = Path(__file__).parent
+
 
 @pytest.fixture(scope="module")
 def df_path():
-    return BASE_PATH.parents[2] / "datasets/data/merges/codecomplex+neetcode+leetcode_clean/full_no_exponential+factorial/full_no_exponential+factorial.csv"
+    return (
+        BASE_LOCATION.parents[2]
+        / "data/data/merges/codecomplex+neetcode+leetcode_clean/full_no_exponential+factorial/full_no_exponential+factorial.csv"
+    )
+
 
 @pytest.fixture(scope="module")
 def df(df_path):
     return pd.read_csv(df_path)
+
 
 def test_dataset(df):
     """Test dataset quality and integrity"""
@@ -35,28 +41,16 @@ def test_dataset(df):
 
     # Add Expectations
     column_list = ["code", "complexity"]
-    suite.add_expectation(
-        gx.expectations.ExpectTableColumnsToMatchOrderedList(column_list=column_list)
-    )
+    suite.add_expectation(gx.expectations.ExpectTableColumnsToMatchOrderedList(column_list=column_list))
     complexities = ["O(1)", "O(logn)", "O(n)", "O(nlogn)", "O(n ^ 2)", "O(n ^ 3)", "np"]
     suite.add_expectation(
         gx.expectations.ExpectColumnValuesToBeInSet(column="complexity", value_set=complexities)
     )
-    suite.add_expectation(
-        gx.expectations.ExpectColumnValuesToBeUnique(column="code")
-    )
-    suite.add_expectation(
-        gx.expectations.ExpectColumnValuesToNotBeNull(column="code")
-    )
-    suite.add_expectation(
-        gx.expectations.ExpectColumnValuesToNotBeNull(column="complexity")
-    )
-    suite.add_expectation(
-        gx.expectations.ExpectColumnValuesToBeOfType(column="code", type_="str")
-    )
-    suite.add_expectation(
-        gx.expectations.ExpectColumnValuesToBeOfType(column="complexity", type_="str")
-    )
+    suite.add_expectation(gx.expectations.ExpectColumnValuesToBeUnique(column="code"))
+    suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="code"))
+    suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="complexity"))
+    suite.add_expectation(gx.expectations.ExpectColumnValuesToBeOfType(column="code", type_="str"))
+    suite.add_expectation(gx.expectations.ExpectColumnValuesToBeOfType(column="complexity", type_="str"))
 
     # Add the Expectation Suite to Context
     context.suites.add(suite)
@@ -64,6 +58,61 @@ def test_dataset(df):
     # Validate the Data against the Suite
     validation_results = batch.validate(suite)
     # Validate
-    assert validation_results['success']
+    assert validation_results["success"]
 
-    
+
+@pytest.fixture
+def dataset_paths():
+    """Real set of data paths"""
+
+    DATASET_PATHS = {
+        "local": {
+            "train": BASE_LOCATION.parents[2]
+            / "data/data/merges/codecomplex+neetcode+leetcode_clean/full_no_exponential+factorial"
+            / "train_set.csv",
+            "eval": BASE_LOCATION.parents[2]
+            / "data/data/merges/codecomplex+neetcode+leetcode_clean/full_no_exponential+factorial"
+            / "eval_set.csv",
+        },
+        "local_two": {"train": "train_set.csv", "eval": "eval_set.csv"},
+        "local_three": {
+            "train": "drive/MyDrive/fine_tuning/train_set.csv",
+            "eval": "drive/MyDrive/fine_tuning/eval_set.csv",
+        },
+        "kaggle": {
+            "train": "/kaggle/input/python-codes-time-complexity/train_set.csv",
+            "eval": "/kaggle/input/python-codes-time-complexity/eval_set.csv",
+        },
+    }
+    return DATASET_PATHS
+
+
+@pytest.fixture
+def expected_data_paths():
+    """Expected data to get from testing find_paths function"""
+
+    return Path(
+        "/Users/koval/dev/biggitybiggityO/data/data/merges/codecomplex+neetcode+leetcode_clean/full_no_exponential+factorial/train_set.csv"
+    ), Path(
+        "/Users/koval/dev/biggitybiggityO/data/data/merges/codecomplex+neetcode+leetcode_clean/full_no_exponential+factorial/eval_set.csv"
+    )
+
+
+def test_find_paths(dataset_paths, expected_data_paths):
+    """Testing find_paths function"""
+
+    # Load paths up
+    train_set_path, eval_set_path = find_paths(dataset_paths)
+    # Evaluate
+    assert train_set_path, eval_set_path == expected_data_paths
+
+
+@pytest.fixture
+def expected_prompt():
+    return "\n            Classify the code snippet into: O(1), O(logn), O(n), O(nlogn),\n              O(n ^ 2), O(n ^ 3), np. And return the answer as the corresponding\n                big O time complexity label.\n            Code: from math import sqrt\n\na, v = map(int, input().split())\nl, d, w = map(int, input().split())\n\ndef findt(u, v, a, dist):\n\tfront = (v*v-u*u)/(2*a)\n\tif front > dist:\n\t\treturn (sqrt(u*u+2*a*dist)-u)/a\n\treturn (v-u)/a + (dist-front)/v\n\ndef solve(a, v, l, d, w):\n\tif v <= w or 2*a*d <= w*w:\n\t\treturn findt(0, v, a, l)\n\tafter = findt(w, v, a, l-d)\n\tpeak = sqrt(a*d + w*w/2)\n\tif peak > v:\n\t\ttravel = (v*v-w*w/2)/a\n\t\tbefore = (2*v-w)/a + (d-travel)/v\n\telse:\n\t\tbefore = (2*peak-w)/a\n\treturn before + after\n\nprint(f'{solve(a, v, l, d, w):.8f}')"
+
+
+def test_generate_prompt(df, expected_prompt):
+    sample_to_test = df.iloc[:1]
+    got = sample_to_test.apply(generate_prompt, axis=1)
+    assert got['code'].iloc[0] == expected_prompt

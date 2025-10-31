869e06efbaad574085919acd876bb1e35f4b073c
komaksym <kovalmaksym2@gmail.com>
2025-10-31 11:02:05 +0100

    move scraping tests to separate tests folder

---
+++ tests/scraping/leetcode/test_clean/mock_files/mock_subfolder1/bitwise-and-of-numbers-range.py
@@ -0,0 +1,17 @@
+# Time:  O(1)
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        while m < n:
+            n &= n - 1
+        return n
+
+
+class Solution2(object):
+    def rangeBitwiseAnd(self, m, n):
+        i, diff = 0, n-m
+        while diff:
+            diff >>= 1
+            i += 1
+        return n & m >> i << i
+
---
+++ tests/scraping/leetcode/test_utils/test_utils.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+
+from src.scraping.leetcode_solutions.src.utils import MyDict, search_files
+
+BASE_LOCATION: Path = Path(__file__).parent
+
+
+@pytest.fixture
+def src_files_path() -> Path:
+    """Returns path to mock source files."""
+    return BASE_LOCATION / "mock_files/"
+
+
+def test_search_files(src_files_path, expected_raw_data) -> None:
+    """Tests search files"""
+    got: MyDict = search_files(src_files_path)
+
+    assert got == expected_raw_data

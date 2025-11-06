65e487a8265c623b5e843c5a785301724d014b8c
komaksym <kovalmaksym2@gmail.com>
2025-11-06 18:49:22 +0000

    add frontend rendering

--- src/app/main.py
+++ src/app/main.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from .inference import predict
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"Health": "OK"}
+BASE_DIR = Path(__file__).parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
-@app.get("/predict")
-def run(inputs: str):
-    response = predict(inputs)
+class CodeInput(BaseModel):
+    code: str
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/predict")
+def predict_complexity(input_data: CodeInput):
+    response = predict(input_data.code)
     return {"label": response}
